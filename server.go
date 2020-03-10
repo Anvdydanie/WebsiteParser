6e369724388bb9b62064caebf4b02f90f4a70a63
a.vodolazov <a.vodolazov@ruru.ru>
2020-03-10 14:50:46 +0300

    v1.4 - добавлены переменные окружения

--- server.go
+++ server.go
@@ -4,10 +4,19 @@ import (
 	"RobotChecker/handlers"
 	"fmt"
 	"net/http"
+	"os"
 )
 
+/*
+При запуске необходимо указывать
+GO_PORT
+NODE_PORT
+REDIS_ADDR
+Пример:
+GO_PORT=9999 NODE_PORT=9090 REDIS_ADDR=localhost:6379 go run server.go
+*/
 func main() {
-	server := &http.Server{Addr: ":9999"}
+	server := &http.Server{Addr: ":" + os.Getenv("GO_PORT")}
 
 	http.HandleFunc("/start-check", handlers.StartCheckHandler)
 
