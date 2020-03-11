79fc9de273471f536083106dbfb45530d8596249
a.vodolazov <a.vodolazov@ruru.ru>
2020-03-11 11:31:21 +0300

    v1.52 log path env

--- logger/logger.go
+++ logger/logger.go
@@ -10,7 +10,7 @@ func Logger(msg string) {
 	currentTime := time.Now()
 	dateForLog := currentTime.Format("2006_01_02")
 
-	file, err := os.OpenFile("./go-parser/go_logs_"+dateForLog+".log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	file, err := os.OpenFile(os.Getenv("LOG_PATH")+"go_logs_"+dateForLog+".log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
