90b76431c91d86db595a476d0d2f93258bec48af
a.vodolazov <a.vodolazov@ruru.ru>
2020-03-11 14:04:36 +0300

    v1.52 log path env

--- server.go
+++ server.go
@@ -13,7 +13,7 @@ GO_PORT
 NODE_PORT
 REDIS_ADDR
 Пример:
-GO_PORT=9999 NODE_PORT=9090 REDIS_ADDR=localhost:6379 go run server.go
+GO_PORT=9999 NODE_PORT=9090 REDIS_ADDR=localhost:6379 LOGS_PATH=./logger/ go run server.go
 */
 func main() {
 	server := &http.Server{Addr: ":" + os.Getenv("GO_PORT")}
